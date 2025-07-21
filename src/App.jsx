19bfacc448d33400404a3ae73ad6f25a813c24e9
Paul Carlson <pdcarlson06@gmail.com>
2025-07-21 19:01:54 -0400

    fix(style): update h1 tag to include gradient text effect

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
       <div className='pattern' />
       <div className='wrapper'>
         <header>
-          <h1>Find Movies You'll Enjoy Without the Hassle</h1>
+          <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
         </header>
       </div>
     </main>
