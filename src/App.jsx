1aec64cde90b61fa47f241fcd7d4b58ef406329f
Paul Carlson <pdcarlson06@gmail.com>
2025-07-28 10:27:28 -0400

    implement Appwrite database and set up handling for updating database entries

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useDebounce } from 'react-use';
 import Search from './components/Search.jsx';
 import Spinner from './components/Spinner.jsx';
 import MovieCard from './components/MovieCard.jsx';
+import { updateSearchCount } from './appwrite.js';
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -49,6 +50,12 @@ const App = () => {
       }
       
       setMoviesList(data.results || []);
+
+      // update database based on search query
+      if (query && data.results.length > 0) {
+        await updateSearchCount(query, data.results[0]);
+      }
+
     } catch (error) {
       console.error("Error fetching movies:", error);
       setErrorMessage("Failed to fetch movies. Please try again later.");
